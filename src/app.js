3c1d33a99304311067e0a936b19bc260bc452557
Alex Marshall Braden <1patientpadawan@gmail.com>
2020-02-28 13:37:46 -0700

    updated CORS policy to allow local dev

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'dev';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors({
-  origin: CLIENT_ORIGIN || 'http://localhost:3000',
+  // origin: CLIENT_ORIGIN || 'http://localhost:3000',
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
